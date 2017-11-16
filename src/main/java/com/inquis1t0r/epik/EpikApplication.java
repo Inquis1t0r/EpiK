96f7d554dfefe93f35c1444cf48943aedd4dc1e5
inquis1t0r <pi0tr3k9292@gmail.com>
2017-11-16 23:02:06 +0100

    initial configuration files

---
+++ src/main/java/com/inquis1t0r/epik/EpikApplication.java
@@ -0,0 +1,12 @@
+package com.inquis1t0r.epik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpikApplication.class, args);
+	}
+}
