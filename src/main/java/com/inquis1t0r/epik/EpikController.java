864f07e251cd274856b523bcc3556b9a5543a45c
inquis1t0r <pi0tr3k9292@gmail.com>
2018-08-19 10:54:25 +0200

    test commit

---
+++ src/main/java/com/inquis1t0r/epik/EpikController.java
@@ -0,0 +1,10 @@
+package com.inquis1t0r.epik;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EpikController {
+}
